4c42e28a2009e94cbb79be7a530f47e20c1fc0b2
danielmoi <daniel@ampersandmoi.com>
2016-05-10 08:30:21 +1000

    add basic dj page

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
 
   get 'dance-2d' => 'pages#dance-2d'
+  get 'dj' => 'pages#dj'
   get 'play' => 'pages#play'
   get 'sounds-3d' => 'pages#sounds-3d'
   get 'instrument' => 'pages#instrument'
