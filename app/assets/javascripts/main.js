630f9774d798a410b13e04ed4030f70824fca780
danielmoi <daniel@ampersandmoi.com>
2016-05-11 13:53:14 +1000

    toggle footer menu on click (properly)

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,11 +2,5 @@ $(document).ready(function() {
   $('.footer__nav-toggle').on('click', function() {
     console.log('hi');
     $('.nav__container').toggle();
-  });  
-});
-
-
-$('.footer__list-item').on('click', function() {
-  console.log('hi');
-  $('.nav__container').toggle();
+  });
 });
