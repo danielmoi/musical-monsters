cc730b244e4fb4af19d1d64e4657dd22761654df
danielmoi <daniel@ampersandmoi.com>
2016-05-12 16:30:13 +1000

    restrict DJ and Drum monsters to logged-in users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,20 +3,32 @@ class PagesController < ApplicationController
   end
 
   def drums
+    if @current_user.nil?
+      flash[:error] = 'You must be logged in to see Drum Monster'
+      redirect_to login_path
+    end
   end
 
   def piano
+
   end
 
   def dj
+    if @current_user.nil?
+      flash[:error] = 'You must be logged in to see DJ Monster'
+      redirect_to login_path
+    end
   end
 
   def dance
+
   end
 
   def snapshot
+
   end
 
   def cubes
+
   end
 end
